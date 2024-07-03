6e80b18556da9436c4a9bfd97e2a67c102c6910a
junscience <popenko.antony@gmail.com>
2024-07-03 17:47:26 +0300

    repaired setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='',
+    name='PBI_telegrambot',
     version='0.0.1',
     author='junscience',
     description='Embedding app for classification',
